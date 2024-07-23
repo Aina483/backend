de8051437cb57bf94918eededba8043f0fb1a807
Aina <ainakhanna0408@gmail.com>
2024-07-23 18:54:10 +0530

    corrected the function name

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,11 +1,13 @@
 //higher order function
 //takes function as an argument and also return function as an argument
-const asyncHnadler=(requestHandler)=>{
+const asyncHandler=(requestHandler)=>{
    return (req,res,next)=>{
     Promise.resolve(requestHandler(req,res,next))
     .catch((err)=>{next(err)});
    }
 }
+
+export default asyncHandler;
 
 
 // const asyncHandler=(fn)=>async(req,res,next)=>{
